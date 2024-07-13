fe32026308a609320e1b942a6d0cde4b564978b1
username <useremail>
2024-07-13 09:37:26 -0700

    2nd Commit(cleanupcode)

--- IterationAndRecursion/IterationAndRecursion.py
+++ IterationAndRecursion/IterationAndRecursion.py
@@ -1,17 +1,52 @@
 # Kyle Stewart CIS261 Lab: Iterative and Recursive Functionality
-def factor(x):
-    print("Factorial results using an iterative function")
+def factor_i(x):
     result = 1
     for i in range(1, x + 1):
         result *= i
-        print(result)
     return result
 
-
-def factor(x):
-    print("Factorial results using a recurive function")
+def factor_r(x):
     if x == 0:
         return 1
     else:
-        return n * factor2(n  -1)
+        return x * factor_r(x - 1)
 
+print("Factorial results using an iterative function")
+u = 0
+result = factor_i(u)
+print(f"{u}! = {result}")
+u = 5
+result = factor_i(u)
+print(f"{u}! = {result}")
+u = 10
+result = factor_i(u)
+print(f"{u}! = {result}")
+u = 25
+result = factor_i(u)
+print(f"{u}! = {result}")
+u = 50
+result = factor_i(u)
+print(f"{u}! = {result}")
+u = 100
+result = factor_i(u)
+print(f"{u}! = {result}")
+
+print("Factorial results using an recursive function")
+u = 0
+result = factor_r(u)
+print(f"{u}! = {result}")
+u = 5
+result = factor_r(u)
+print(f"{u}! = {result}")
+u = 10
+result = factor_r(u)
+print(f"{u}! = {result}")
+u = 25
+result = factor_r(u)
+print(f"{u}! = {result}")
+u = 50
+result = factor_r(u)
+print(f"{u}! = {result}")
+u = 100
+result = factor_r(u)
+print(f"{u}! = {result}")
